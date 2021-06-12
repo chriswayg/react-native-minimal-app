1a90111be6216b1de72fa692ba927996ee35ec54
chriswayg <chriswayg@gmail.com>
2021-06-12 13:36:43 +0800

    using index.js from standard template

--- template/index.js
+++ template/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import { AppRegistry, Text } from 'react-native';
+import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
-
-const App = function() {
-  return (
-       <Text style={{fontSize: 32}}>Hello, World!</Text>
-  );
-}
 
 AppRegistry.registerComponent(appName, () => App);
