e01bf5c626d357b444793534fea55c10813f8d22
chriswayg <chriswayg@gmail.com>
2021-06-12 13:37:26 +0800

    added config and test files from standard template

---
+++ template/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 32 }}>Hello, world!</Text>;
+    </SafeAreaView>
+  );
+}
