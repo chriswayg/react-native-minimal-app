9a4479fe089d6e336a15738080689dfb79b71978
chriswayg <chriswayg@gmail.com>
2021-06-12 13:51:50 +0800

    improve Hello World screen

--- template/App.js
+++ template/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import {Text, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text style={{ fontSize: 32 }}>Hello, world!</Text>;
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 32}}>Hello, world!</Text>
     </SafeAreaView>
   );
 }
