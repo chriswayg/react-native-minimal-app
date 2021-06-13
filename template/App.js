908ab676aa314a0075b57bea74795faf8b4cfe76
chriswayg <chriswayg@gmail.com>
2021-06-13 17:48:21 +0800

    add quote

--- template/App.js
+++ template/App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <SafeAreaView
       style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 32}}>Hello, world!</Text>
+      <Text style={{fontSize: 32, textAlign: 'center'}}>
+        The price of freedom is eternal vigilance.
+      </Text>
     </SafeAreaView>
   );
 }
